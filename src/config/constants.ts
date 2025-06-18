1184044228863f564dc10a1276de2d87ba12417c
contrasanta <srihari221122@gmail.com>
2025-06-18 19:56:01 +0530

    base url is now fetched from .env

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,4 +1,4 @@
-export const API_BASE_URL = "http://192.168.210.176:3000/api";
+export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export const API_ENDPOINTS = {
   GENERATE_ID: `${API_BASE_URL}/generate-id`,
